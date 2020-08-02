fc7693f47ce6c030a00fc5edfaa8e1792cbce0b3
Nahuel Villarreal <55802619+NahuelVillarreal@users.noreply.github.com>
2020-08-02 01:16:59 -0300

    Insert Datos Prueba
    Se insertan valores de prueba para comenzar a trabajar en la API

--- tooling.sql
+++ tooling.sql
@@ -20,10 +20,9 @@ CREATE TABLE Elemento_reparado(
     nom_elemento_reparado VARCHAR(50) NOT NULL
 )
 
-CREATE TABLE Herramienta_reparacion(
-    id_herramienta INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    nom_herramienta VARCHAR(50) NOT NULL
-)
+--CREATE TABLE Herramienta_reparacion(
+--    id_herramienta INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+--    nom_herramienta VARCHAR(50) NOT NULL)
 
 CREATE TABLE  Medida(
     id_medida INT NOT NULL PRIMARY KEY IDENTITY(1,1),
@@ -44,13 +43,91 @@ CREATE TABLE Intervencion(
     fecha DATE NOT NULL, --Segun el relevamiento las intervenciones no duran mas de un dia
     tipo_reparacion INT NOT NULL,
     elemento_reparado INT NOT NULL,
-    herramienta_reparacion INT NOT NULL,
+    --herramienta_reparacion INT NOT NULL,
     medida INT NOT NULL,
     disenio INT NOT NULL,
     CONSTRAINT FK_tipo_reparacion FOREIGN KEY (tipo_reparacion) REFERENCES Tipo_reparacion(id_tipo_reparacion),
     CONSTRAINT FK_elemento_reparado FOREIGN KEY (elemento_reparado) REFERENCES Elemento_reparacion(id_tipo_reparacion),
-    CONSTRAINT FK_herramienta_reparacion FOREIGN KEY (herramienta_reparacion) REFERENCES Herramienta_reparacion(id_herramienta),
+    --CONSTRAINT FK_herramienta_reparacion FOREIGN KEY (herramienta_reparacion) REFERENCES Herramienta_reparacion(id_herramienta),
     CONSTRAINT FK_medida FOREIGN KEY (medida) REFERENCES Medida(id_medida),
     CONSTRAINT FK_disenio FOREIGN KEY (disenio) REFERENCES Disenio(id_disenio) 
 )
 
+--Nombre prueba
+INSERT INTO Operario(nombre)
+VALUES('Juan Perez'),
+('Pedro Peligro'),
+('Juan Carlos'),
+('Roberto Rubino'),
+('Tatiana Tutei'),
+('Alfonso rivero'),
+('Jorge Rodriguez')
+
+INSERT INTO Tipo_reparacion(nom_tipo_reparacion)
+VALUES('ASIENTO DE ARO'),
+('FLASH'),
+('CAMBIO DE ESTAMPADO'),
+('MOLDE NUEVO'),
+('REPERFILADO'),
+('GRABADO'),
+('AJUSTE'),
+('REPARACIÓN GENERAL'),
+('COLOCAR INSERTO'),
+('PREVENTIVO'),
+('TORNEADO'),
+('FRESADO'),
+('SOLDADO'),
+('AGREGADO DE VENTILACIONES'),
+('3S'),
+('KAIZEN'),
+('OTRO')
+
+
+INSERT INTO Elemento_reparado(nom_elemento_reparado)
+VALUES ('MOLDE'),
+('ARO'),
+('TAMBOR'),
+('TOOLING ARMADO'),
+('STEELASTIC'),
+('TUO'),
+('KOKUSAI'),
+('RIM FLOW'),
+('MONOWIRE'),
+('CONTENEDOR'),
+('OTRO DEPARTAMENTO'),
+('OTROS')
+
+--INSERT INTO Herramienta_reparacion(nom_herramienta)
+--VALUES 
+
+INSERT INTO Medida(nom_medida)
+VALUES('13'),
+('14'),
+('15'),
+('16'),
+('17'),
+('18'),
+('19'),
+('20'),
+('21'),
+('22'),
+('24'),
+('26'),
+('28'),
+('30'),
+('32'),
+('34'),
+('36'),
+('38'),
+('40'),
+('42'),
+('44'),
+('46')
+
+INSERT INTO Disenio(nom_medida)
+VALUES ('C104'),
+('C104V'),
+('C105'),
+('C105V'),
+('C203'),
+('C203V')
